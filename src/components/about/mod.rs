32e1f88cfddf2c8b215768d417c9b2d378a0852b
Esteban Borai <estebanborai@gmail.com>
2020-10-22 21:29:27 -0300

    feat: accordion component

--- src/components/about/mod.rs
+++ src/components/about/mod.rs
@@ -1,3 +1,4 @@
+use crate::components::accordion::Accordion;
 use crate::components::title::Title;
 
 use yew::prelude::*;
@@ -33,15 +34,14 @@ impl Component for About {
                   }
                 </p>
               </article>
-              <article id="code">
-                <Title href="code" text="Código de Conducta" />
+              <Accordion title="Código de Conducta" href="#code">
                 <p>
                   {
                     r#"Esperamos que cualquier participante de esta organización y los medios acompañados
-                esten enterados del siguiente código de conducta y ayuden a cumplir el mismo.
-                Llámese participante de esta organización a cualquier colaborador de la organización en GitHub,
-                miembro del chat en Telegram, contribuidor que no sea miembro de la organización, asi como cualquier
-                medio de comunicación o participación que es representado por esta comunidad."#
+                    esten enterados del siguiente código de conducta y ayuden a cumplir el mismo.
+                    Llámese participante de esta organización a cualquier colaborador de la organización en GitHub,
+                    miembro del chat en Telegram, contribuidor que no sea miembro de la organización, asi como cualquier
+                    medio de comunicación o participación que es representado por esta comunidad."#
                   }
                   <hr />
                   { "Se requiere que todo participante de la organización " }
@@ -51,17 +51,17 @@ impl Component for About {
                   <br />
                   {
                     r#"Todos los participantes de la organización tanto contribuidores como no contribuidores deben ser
-                tratados con respeto y ser libres de cualquier discriminación, así como intimidación, acoso o victimización.
-                No será tolerado ningún tipo de contenido o comportamiento obseno e inadecuado de cualquier naturaleza,
-                sean imágenes inadecuadas, o lenguaje inapropiado."#
+                    tratados con respeto y ser libres de cualquier discriminación, así como intimidación, acoso o victimización.
+                    No será tolerado ningún tipo de contenido o comportamiento obseno e inadecuado de cualquier naturaleza,
+                    sean imágenes inadecuadas, o lenguaje inapropiado."#
                   }
                   {
                     r#"Si algún participante se ve envuelto en alguno de los comportamientos antes mencionados, los administradores
-                de la comunidad en cuestión tendrán derecho a tomar acciones que crean pertinentes hasta la expulsión de todo
-                medio relacionado a la misma."#
+                    de la comunidad en cuestión tendrán derecho a tomar acciones que crean pertinentes hasta la expulsión de todo
+                    medio relacionado a la misma."#
                   }
                 </p>
-              </article>
+              </Accordion>
             </div>
           </section>
         }
